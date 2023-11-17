4847a2b512809781815ff8c65e99c1fc11b69a51
Mirha Masala <mirha@hey.com>
2023-11-17 10:21:08 +0000

    Responsify header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,12 +68,12 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center gap-24">
           <header className="flex gap-5 flex-col items-center text-center">
             <span className="font-semibold text-indigo-600">UXIT</span>
-            <h1 className="text-6xl font-extrabold">
+            <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">
               UXIT User-Focused Solutions for the
               <br />
               <span className="text-indigo-600">Future of Filecoin</span>
             </h1>
-            <p className="text-gray-200">
+            <p className="text-lg leading-8 text-gray-200">
               The FF UX Improvement Team is dedicated to finding user-focused
               solutions for the future of Filecoin.
             </p>
